6447a8fca2d98c04a9529932c1cecda03f99c166
Jonathan Stern <jstern718@gmail.com>
2023-04-11 20:38:32 -0400

    remove images button functionality

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 "use strict";
 
+const API_KEY = "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym";
+const BASE_URL = "http://api.giphy.com/v1/gifs";
+
 let inputForm = $("#form");
 
 /** handles what happens when submitting Giphy key */
@@ -10,13 +13,21 @@ function handleSubmit(evt) {
   addGiphy(searchVal);
 }
 
-/** takes in keyText and sends a request to GIPHY API and adds
+/** takes in searchTerm and sends a request to GIPHY API and adds
  *  a new div with gif
  */
-async function addGiphy(keyText) {
+async function addGiphy(searchTerm) {
   let newDiv = $("<div>");
   let newGif = $("<img>");
-  let giphy = await axios.get("http://api.giphy.com/v1/gifs/search", {params: {q:keyText, api_key:"MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"}});
+  let giphy = await axios.get(
+    `${BASE_URL}/search`,
+    {
+      params: {
+        q: searchTerm,
+        api_key: API_KEY
+      }
+    }
+  );
   console.log(giphy.data.data['0']);
   let giphyLink = giphy.data.data['0'].images.original.url;
   newGif.attr("src", giphyLink);
@@ -24,7 +35,13 @@ async function addGiphy(keyText) {
   $("#gifs").append(newDiv);
 }
 
+/** removes all gifs */
+function removeAllGifs(){
+  $("#gifs").empty();
+}
 
+const removeButton = $("#removeButt");
+removeButton.on("click", removeAllGifs);
 
 inputForm.on("submit", handleSubmit)
 
